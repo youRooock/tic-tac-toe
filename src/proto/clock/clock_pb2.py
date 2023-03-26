8353551e99450cd9d76bda0e0fee0b1e5a3a3fd5
Mykyta Skliarov <nikitaskliarov66@gmail.com>
2023-03-26 15:51:16 +0300

    pep8

--- src/proto/clock/clock_pb2.py
+++ src/proto/clock/clock_pb2.py
@@ -6,6 +6,7 @@ from google.protobuf.internal import builder as _builder
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
+
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -14,17 +15,18 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0b\x63lock.proto\x1a\x1bgoogle/protobuf/empty.proto\"$\n\x12\x41\x64justClockRequest\x12\x0e\n\x06offset\x18\x01 \x01(\x01\"\'\n\x12\x46\x65tchClockResponse\x12\x11\n\ttimestamp\x18\x01 \x01(\x01\x32\x8b\x01\n\x0c\x43lockService\x12<\n\x0b\x66\x65tch_clock\x12\x16.google.protobuf.Empty\x1a\x13.FetchClockResponse\"\x00\x12=\n\x0c\x61\x64just_clock\x12\x13.AdjustClockRequest\x1a\x16.google.protobuf.Empty\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
+    b'\n\x0b\x63lock.proto\x1a\x1bgoogle/protobuf/empty.proto"$\n\x12\x41\x64justClockRequest\x12\x0e\n\x06offset\x18\x01 \x01(\x01"\'\n\x12\x46\x65tchClockResponse\x12\x11\n\ttimestamp\x18\x01 \x01(\x01\x32\x8b\x01\n\x0c\x43lockService\x12<\n\x0b\x66\x65tch_clock\x12\x16.google.protobuf.Empty\x1a\x13.FetchClockResponse"\x00\x12=\n\x0c\x61\x64just_clock\x12\x13.AdjustClockRequest\x1a\x16.google.protobuf.Empty"\x00\x62\x06proto3'
+)
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'clock_pb2', globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "clock_pb2", globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
-
-  DESCRIPTOR._options = None
-  _ADJUSTCLOCKREQUEST._serialized_start=44
-  _ADJUSTCLOCKREQUEST._serialized_end=80
-  _FETCHCLOCKRESPONSE._serialized_start=82
-  _FETCHCLOCKRESPONSE._serialized_end=121
-  _CLOCKSERVICE._serialized_start=124
-  _CLOCKSERVICE._serialized_end=263
+    DESCRIPTOR._options = None
+    _ADJUSTCLOCKREQUEST._serialized_start = 44
+    _ADJUSTCLOCKREQUEST._serialized_end = 80
+    _FETCHCLOCKRESPONSE._serialized_start = 82
+    _FETCHCLOCKRESPONSE._serialized_end = 121
+    _CLOCKSERVICE._serialized_start = 124
+    _CLOCKSERVICE._serialized_end = 263
 # @@protoc_insertion_point(module_scope)
--- src/proto/clock/clock_pb2_grpc.py
+++ src/proto/clock/clock_pb2_grpc.py
@@ -16,15 +16,15 @@ class ClockServiceStub(object):
             channel: A grpc.Channel.
         """
         self.fetch_clock = channel.unary_unary(
-                '/ClockService/fetch_clock',
-                request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-                response_deserializer=clock__pb2.FetchClockResponse.FromString,
-                )
+            "/ClockService/fetch_clock",
+            request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            response_deserializer=clock__pb2.FetchClockResponse.FromString,
+        )
         self.adjust_clock = channel.unary_unary(
-                '/ClockService/adjust_clock',
-                request_serializer=clock__pb2.AdjustClockRequest.SerializeToString,
-                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
-                )
+            "/ClockService/adjust_clock",
+            request_serializer=clock__pb2.AdjustClockRequest.SerializeToString,
+            response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+        )
 
 
 class ClockServiceServicer(object):
@@ -33,68 +33,91 @@ class ClockServiceServicer(object):
     def fetch_clock(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
+        context.set_details("Method not implemented!")
+        raise NotImplementedError("Method not implemented!")
 
     def adjust_clock(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
+        context.set_details("Method not implemented!")
+        raise NotImplementedError("Method not implemented!")
 
 
 def add_ClockServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'fetch_clock': grpc.unary_unary_rpc_method_handler(
-                    servicer.fetch_clock,
-                    request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
-                    response_serializer=clock__pb2.FetchClockResponse.SerializeToString,
-            ),
-            'adjust_clock': grpc.unary_unary_rpc_method_handler(
-                    servicer.adjust_clock,
-                    request_deserializer=clock__pb2.AdjustClockRequest.FromString,
-                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-            ),
+        "fetch_clock": grpc.unary_unary_rpc_method_handler(
+            servicer.fetch_clock,
+            request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+            response_serializer=clock__pb2.FetchClockResponse.SerializeToString,
+        ),
+        "adjust_clock": grpc.unary_unary_rpc_method_handler(
+            servicer.adjust_clock,
+            request_deserializer=clock__pb2.AdjustClockRequest.FromString,
+            response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+        ),
     }
-    generic_handler = grpc.method_handlers_generic_handler(
-            'ClockService', rpc_method_handlers)
+    generic_handler = grpc.method_handlers_generic_handler("ClockService", rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
 
 
- # This class is part of an EXPERIMENTAL API.
+# This class is part of an EXPERIMENTAL API.
 class ClockService(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
-    def fetch_clock(request,
+    def fetch_clock(
+        request,
+        target,
+        options=(),
+        channel_credentials=None,
+        call_credentials=None,
+        insecure=False,
+        compression=None,
+        wait_for_ready=None,
+        timeout=None,
+        metadata=None,
+    ):
+        return grpc.experimental.unary_unary(
+            request,
             target,
-            options=(),
-            channel_credentials=None,
-            call_credentials=None,
-            insecure=False,
-            compression=None,
-            wait_for_ready=None,
-            timeout=None,
-            metadata=None):
-        return grpc.experimental.unary_unary(request, target, '/ClockService/fetch_clock',
+            "/ClockService/fetch_clock",
             google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             clock__pb2.FetchClockResponse.FromString,
-            options, channel_credentials,
-            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+        )
 
     @staticmethod
-    def adjust_clock(request,
+    def adjust_clock(
+        request,
+        target,
+        options=(),
+        channel_credentials=None,
+        call_credentials=None,
+        insecure=False,
+        compression=None,
+        wait_for_ready=None,
+        timeout=None,
+        metadata=None,
+    ):
+        return grpc.experimental.unary_unary(
+            request,
             target,
-            options=(),
-            channel_credentials=None,
-            call_credentials=None,
-            insecure=False,
-            compression=None,
-            wait_for_ready=None,
-            timeout=None,
-            metadata=None):
-        return grpc.experimental.unary_unary(request, target, '/ClockService/adjust_clock',
+            "/ClockService/adjust_clock",
             clock__pb2.AdjustClockRequest.SerializeToString,
             google_dot_protobuf_dot_empty__pb2.Empty.FromString,
-            options, channel_credentials,
-            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+        )
